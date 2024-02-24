b3924be8fe26828cc8b6fe32f5abf85cecb45f56
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-02-24 11:11:07 +0100

    Algorithm 01 v3.0

--- Algorithms/01.Binary Search.py
+++ Algorithms/01.Binary Search.py
@@ -16,7 +16,31 @@ https://www.geeksforgeeks.org/binary-search/
 
 """
 
-# def binarySearch_Iter(arr, trg):
+def binarySearch_Iter(arr, trg):
+
+    print(f"Array:  {arr}")
+    print(f"Target: {trg}")
+    print()
+
+    lft = 0
+    rgt = len(arr) - 1
+
+    while lft <= rgt:
+
+        mid = lft + (rgt - lft) // 2
+
+        if arr[mid] == trg:
+            print()
+            print(f"Index of target: {mid}")
+            return mid
+
+        elif arr[mid] < trg:
+            lft = mid + 1
+            print(f"\tTarget left  =>  {arr[lft:rgt+1]}")
+
+        else:
+            rgt = mid - 1
+            print(f"\tTarget right =>  {arr[lft:rgt+1]}")
 
 
 if __name__ == '__main__':
