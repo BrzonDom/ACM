f0f5d7a3f2a48678f3c22d4c0d34494c343cfc5a
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-02-24 21:07:16 +0100

    Algorithm 02.py v01.0

---
+++ Algorithms/02.BFS Breadth-First Search.py
@@ -0,0 +1,15 @@
+"""
+Breadth-First Search (BFS) – Iterative and Recursive Implementation
+
+https://www.techiedelight.com/breadth-first-search/
+
+    Breadth–first search (BFS) is an algorithm for traversing or searching
+    tree or graph data structures. It starts at the tree root
+    (or some arbitrary node of a graph, sometimes referred to as a ‘search key’)
+    and explores the neighbor nodes first before moving to the next-level neighbors.
+
+    Breadth–first search (BFS) is a graph traversal algorithm that explores vertices
+    in the order of their distance from the source vertex, where distance is
+    the minimum length of a path from the source vertex to the node.
+
+"""
