7f2189b1c3d1be0da9d6788144ddd20d2f0f901e
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-03-13 16:58:52 +0100

    Algorithm 03.py v01.0

---
+++ Algorithms/03.DFS Depth-First Search.py
@@ -0,0 +1,20 @@
+"""
+Depth-First Search (DFS) – Iterative and Recursive Implementation
+
+https://www.techiedelight.com/depth-first-search/
+
+    Depth–first search (DFS) is an algorithm for traversing or searching
+    tree or graph data structures. One starts at the root
+    (selecting some arbitrary node as the root for a graph) and
+    explore as far as possible along each branch before backtracking.
+
+"""
+
+
+if __name__ == '__main__':
+
+    print("03.DFS Depth-First Search.py\n")
+
+    edges = [(1, 2), (1, 3), (1, 4), (2, 5),
+             (2, 6), (5, 9), (5, 10), (4, 7),
+             (4, 8), (7, 11), (7, 12)]
