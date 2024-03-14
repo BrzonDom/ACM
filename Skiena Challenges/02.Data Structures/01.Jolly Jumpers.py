b707312fcfe1ea123493f12594f6f64baafed6f9
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-03-14 18:59:29 +0100

    Skiena 02.01.py v01.0

---
+++ Skiena Challenges/02.Data Structures/01.Jolly Jumpers.py
@@ -0,0 +1,26 @@
+"""
+
+Jolly Jumpers
+
+    A sequence of n > 0 integers is called a jolly jumper if the absolute values of the
+    differences between successive elements take on all possible values 1 through n − 1.
+
+
+    Inputs:
+        Each line of input contains an integer n < 3, 000 followed by n integers representing the
+        sequence.
+
+    Output:
+        For each line of input generate a line of output saying “Jolly” or “Not jolly”.
+
+
+    Sample:
+
+        1 4 2 3
+            => Jolly
+
+        1 4 2 -1 6
+            => Not Jolly
+
+"""
+
