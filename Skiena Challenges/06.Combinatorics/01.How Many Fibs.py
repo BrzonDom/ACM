9bdba545bd52b728ed418e66276d3ff397148b94
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-03-14 07:02:01 +0100

    Skiena 06.01.py v01.0

---
+++ Skiena Challenges/06.Combinatorics/01.How Many Fibs.py
@@ -0,0 +1,27 @@
+"""
+
+How Many Fibs ?
+
+    Inputs:
+        The input contains several test cases. Each test case consists of two non-negative integer
+        numbers a and b. Input is terminated by a = b = 0. Otherwise, a ≤ b ≤ 10^(100).
+        The numbers a and b are given with no superfluous leading zeros.
+
+
+    Output:
+        For each test case output on a single line the number of Fibonacci numbers f_i with
+        a ≤ f_i ≤ b
+
+
+    Samples:
+
+        10 100
+            => 5
+
+        1234567890 9876543210
+            => 4
+
+        0 0
+            =>
+
+"""
