b3dec36085b7aaaa91d306ee75c75b9ed73fcc19
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-03-16 01:00:15 +0100

    Skiena 02.02.py v02.3

--- Skiena Challenges/02.Data Structures/02.Poker Hands.py
+++ Skiena Challenges/02.Data Structures/02.Poker Hands.py
@@ -76,15 +76,15 @@ Poker Hands
 #     rank2 = False
 #     rank3 = False
 #
-#     pair_Tab = [0]
+#     pair_Tbl = [0]
 #
 #     for c, card in enumerate(hand):
 #
-#         pair_Tab = [0]
+#         pair_Tbl = [0]
 #
 #         for pair in hand[c:]:
 #             if card[0] == card[0]:
-#                 pair_Tab[-1] += 1
+#                 pair_Tbl[-1] += 1
 
 
 
@@ -110,7 +110,23 @@ if __name__ == '__main__':
     org_hands = ["2H", "3D", "5S", "9C", "KD",
                  "2C", "3H", "4S", "8C", "AH"]
 
-    tst_hand = ["2H", "2D", "2S", "5C", "5D"]
+    tstHands_Lst = [
+                    # 0.       High card
+                    ["2H", "4D", "KS", "3C", "6D"],
+                    # 1.       Pair
+                    ["2H", "KD", "3S", "3C", "5D"],
+                    # 2.       Two Pair
+                    ["2H", "2D", "JS", "3C", "3D"],
+                    # 3.       Three of a Kind
+                    ["2H", "2D", "2S", "3C", "5D"],
+                    # 4.       Full House
+                    ["2H", "2D", "2S", "5C", "5D"],
+                    # 5.       Four of a Kind
+                    ["2H", "3D", "3S", "3C", "53"]]
+
+
+    tst_hand = tstHands_Lst[0]
+    # tst_hand = ["2H", "2D", "2S", "5C", "5D"]
 
 
 
@@ -146,41 +162,47 @@ if __name__ == '__main__':
 
     print(f"\n\t\t{hand_test}")
 
-    pair_Tab = [0]
-    info_Tab = [0]
+    pair_Tbl = [0]
+    info_Tbl = [0]
+    card_Srt = [0, 0, 0, 0, 0]
 
-    highCard = 0
+    sortCard = []
     mtch = 0
 
     for crd, card in enumerate(hand_test):
 
+        card_Srt[crd] = card[0]
+
         if mtch > crd:
             continue
 
-        if card[0] > highCard:
-            highCard = card[0]
+        # for s, srt in enumerate(card_Srt):
+        #     if card[0] > srt:
+        #         str = card[0]
 
-        if pair_Tab[-1]:
-            pair_Tab.append(0)
+        if pair_Tbl[-1]:
+            pair_Tbl.append(0)
 
         for pair in hand_test[crd+1:]:
             if pair[0] == card[0]:
-                pair_Tab[-1] += 1
+                pair_Tbl[-1] += 1
                 mtch += 1
 
         mtch += 1
 
-    if pair_Tab[-1] == 0:
-        pair_Tab = pair_Tab[:-1]
+    if pair_Tbl[-1] == 0:
+        pair_Tbl = pair_Tbl[:-1]
 
-    print(f"\n\tSame cards table: {pair_Tab}")
-    # print(pair_Tab)
+    card_Srt.sort(reverse=True)
 
-    if len(pair_Tab) == 1:
-        if pair_Tab[0] == 2:
+    # print(f"\n\tSame cards table: {pair_Tbl}")
+    # print(pair_Tbl)
+
+    if len(pair_Tbl) == 1:
+        if pair_Tbl[0] == 2:
            print(f"\n\tPlayer has Three of a Kind")
 
-        # elif pair_Tab[0] == 1:
+        # elif pair_Tbl[0] == 1:
         else:
             print(f"\n\tPlayer has a Pair")
 
@@ -189,16 +211,16 @@ if __name__ == '__main__':
         #     print(f"\n\tPlayer has a High Card")
 
 
-    elif len(pair_Tab) == 2:
-        if 1 in pair_Tab and 2 in pair_Tab:
+    elif len(pair_Tbl) == 2:
+        if 1 in pair_Tbl and 2 in pair_Tbl:
             print("\n\tPlayer has a FUll House")
 
-        # elif pair_Tab[0] == 1 and pair_Tab[1] == 1:
+        # elif pair_Tbl[0] == 1 and pair_Tbl[1] == 1:
         else:
             print("\n\tPlayer has Two Pairs")
 
-    # elif len(pair_Tab) == 0:
+    # elif len(pair_Tbl) == 0:
     else:
         print(f"\n\tPlayer has a High Card")
 
-    print(f"\t\tHigh Card: {highCard}")
+    print(f"\t\tHigh Card: {card_Srt[0]}")
