d9992396d411b756999cf4205a0e4d9435837de6
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-03-19 18:28:21 +0100

    Skiena 02.04.py v01.1

--- Skiena Challenges/02.Data Structures/04.Crypt Kicker.py
+++ Skiena Challenges/02.Data Structures/04.Crypt Kicker.py
@@ -32,3 +32,36 @@ Crypt Kicker
                **** *** **** *** **** *** **** *** ******
 
 """
+
+# conv = {
+#         'a':'a',
+#         'b':'b',
+#         'c':'c',
+#         'd':'d',
+#         'e':'e',
+#         'f':'f',
+#         'g':'g',
+#         'h':'h',
+#         'i':'i',
+#         'j':'j',
+#         'k':'k',
+#         'l':'l',
+#         'm':'m',
+#         'n':'n',
+#         'o':'o',
+#         'p':'p',
+#         'q':'q',
+#         'r':'r',
+#         's':'s',
+#         't':'t',
+#         'v':'v',
+#         'w':'w',
+#         'x':'x',
+#         'y':'y',
+#         'z':'z'
+#  }
+
+enc_wordsLst = ["and", "dick", "jane", "puff", "spot", "yertle"]
+
+enc_textLst = ["bjvg", "xsb", "hxsn", "xsb", "qymm", "xsb", "rqat", "xsb", "pnetfn"]
+
