27a46771327f5368a71ab6cbfa636eb67b78affb
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-03-21 02:33:54 +0100

    Skiena 02.05.py v02.1

--- Skiena Challenges/02.Data Structures/05.Stack 'em Up.py
+++ Skiena Challenges/02.Data Structures/05.Stack 'em Up.py
@@ -84,6 +84,7 @@ Stack 'em Up
                 3 of Clubs
 
 """
+import copy
 
 Input_Str = """
             1
@@ -100,7 +101,7 @@ Input_Str = """
 Input = list(map(int, Input_Str.split()))
 print("Input string: ", end="\n\t\t\t\"\"\"")
 print(Input_Str, end="\"\"\"\n\n")
-print(f"Input: {Input}")
+print(f"Input: {Input}\n")
 
 Cases = [[] for _ in range(Input[0])]
 Input = Input[1:]
@@ -116,8 +117,14 @@ for c in range(ShfflCnt):
 
 Moves = Input[ShfflCnt * 52:]
 
-print(Moves)
+Prev_Cards = ['2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC', 'AC',
+              '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '10D', 'JD', 'QD', 'KD', 'AD',
+              '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JH', 'QH', 'KH', 'AH',
+              '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', 'JS', 'QS', 'KS', 'AS']
 
+Next_Cards = copy.deepcopy(Prev_Cards)
 
+print("Moves:")
 
-
+for m in Moves:
+    print(f"\t{m:2}: {Shffls[m-1]}")
