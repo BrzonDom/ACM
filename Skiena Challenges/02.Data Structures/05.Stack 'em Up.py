5cc4b31d9254874bd33d2c66f8ee42cb4dc29f5e
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-03-21 04:02:26 +0100

    Skiena 02.05.py v02.5

--- Skiena Challenges/02.Data Structures/05.Stack 'em Up.py
+++ Skiena Challenges/02.Data Structures/05.Stack 'em Up.py
@@ -147,7 +147,7 @@ for c, card in enumerate(Next_Cards):
 
     else:
         print(card, end=", ")
-
+print("\n\n")
 
 Positions = {
      1 :  1,
@@ -204,6 +204,12 @@ Positions = {
     52 : 52
 }
 
+Org_Cards = ['2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC', 'AC',
+             '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '10D', 'JD', 'QD', 'KD', 'AD',
+             '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JH', 'QH', 'KH', 'AH',
+             '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', 'JS', 'QS', 'KS', 'AS']
+
+
 # Positions = [ 1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13,
 #              14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
 #              27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
@@ -216,4 +222,13 @@ Positions = {
 
 # print(Positions)
 
+for m in Moves:
 
+    for pos in Positions:
+
+        prvPos = Positions[pos] - 1
+        nxtPos = Shffls[m-1][prvPos]
+
+        Positions[pos] = nxtPos
+
+print(Positions)
