75c55a0414ac66f7bdca73b205502abb25cc547d
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-03-21 18:39:34 +0100

    Skiena 04.01.py v01.2

--- Skiena Challenges/04.Sorting/01.Vito's Family.py
+++ Skiena Challenges/04.Sorting/01.Vito's Family.py
@@ -11,13 +11,13 @@ Vito's Family
     Input:
         The input consists of several test cases. The first line contains the number of test cases.
         For each test case you will be given the integer number of relatives r (0 < r < 500) and
-        the street numbers (also integers) s1, s2, . . . , si, . . . , sr where they live (0 < si < 30, 000).
+        the street numbers (also integers) s_1, s_2, ..., s_i, ..., s_r where they live (0 < si < 30, 000).
         Note that several relatives might live at the same street number.
 
     Output:
         For each test case, your program must write the minimal sum of distances from the
         optimal Vito’s house to each one of his relatives. The distance between two street
-        numbers si and sj is dij = |si − sj |.
+        numbers s_i and s_j is d_ij = |s_i − s_j|.
 
 
     Sample:
@@ -42,5 +42,11 @@ for case in range(Cases):
     Input_Str = InputRaw_Str.split("\n")[case+2]
     Input = list(map(int, Input_Str.split()))
 
-    print(f"Input: {Input}")
-    print()
+    print(f"Input: {Input}\n")
+
+    Relat = Input[0]
+    Hous = Input[1:]
+
+    print(f"\tRelatives: {Relat}")
+    print(f"\tHouse numbers: {Hous}")
+    print("\n")
