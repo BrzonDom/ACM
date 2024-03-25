92a553c441db460fca6eb1fa97e02024834acc1e
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-03-25 23:06:58 +0100

    Skiena 02.03.py v02.2

--- Skiena Challenges/02.Data Structures/03.Hartals.py
+++ Skiena Challenges/02.Data Structures/03.Hartals.py
@@ -113,3 +113,165 @@ for c in range(Input[0]):
 
     print(f"\n\tDays lost: {lostDays}")
     print("\n")
+
+
+"""__Output__"""
+"""
+Input string:   "2 14 3 3 4 8 100 4 12 15 25 40"
+Input:         [2, 14, 3, 3, 4, 8, 100, 4, 12, 15, 25, 40]
+
+
+	Days: 14
+	Cases: [3, 4, 8]
+
+		 1 1 : Sunday
+		 2 2 : Monday
+		 3 3 : Tuesday
+					X
+		 4 4 : Wednesday
+					X
+		 5 5 : Thursday
+		 6 6 : Friday
+		 7 0 : Saturday
+		 8 1 : Sunday
+					X
+					X
+		 9 2 : Monday
+					X
+		10 3 : Tuesday
+		11 4 : Wednesday
+		12 5 : Thursday
+					X
+					X
+		13 6 : Friday
+
+	Days lost: 5
+
+
+	Days: 100
+	Cases: [12, 15, 25, 40]
+
+		 1 1 : Sunday
+		 2 2 : Monday
+		 3 3 : Tuesday
+		 4 4 : Wednesday
+		 5 5 : Thursday
+		 6 6 : Friday
+		 7 0 : Saturday
+		 8 1 : Sunday
+		 9 2 : Monday
+		10 3 : Tuesday
+		11 4 : Wednesday
+		12 5 : Thursday
+					X
+		13 6 : Friday
+		14 0 : Saturday
+		15 1 : Sunday
+					X
+		16 2 : Monday
+		17 3 : Tuesday
+		18 4 : Wednesday
+		19 5 : Thursday
+		20 6 : Friday
+		21 0 : Saturday
+		22 1 : Sunday
+		23 2 : Monday
+		24 3 : Tuesday
+					X
+		25 4 : Wednesday
+					X
+		26 5 : Thursday
+		27 6 : Friday
+		28 0 : Saturday
+		29 1 : Sunday
+		30 2 : Monday
+					X
+		31 3 : Tuesday
+		32 4 : Wednesday
+		33 5 : Thursday
+		34 6 : Friday
+		35 0 : Saturday
+		36 1 : Sunday
+					X
+		37 2 : Monday
+		38 3 : Tuesday
+		39 4 : Wednesday
+		40 5 : Thursday
+					X
+		41 6 : Friday
+		42 0 : Saturday
+		43 1 : Sunday
+		44 2 : Monday
+		45 3 : Tuesday
+					X
+		46 4 : Wednesday
+		47 5 : Thursday
+		48 6 : Friday
+		49 0 : Saturday
+		50 1 : Sunday
+					X
+		51 2 : Monday
+		52 3 : Tuesday
+		53 4 : Wednesday
+		54 5 : Thursday
+		55 6 : Friday
+		56 0 : Saturday
+		57 1 : Sunday
+		58 2 : Monday
+		59 3 : Tuesday
+		60 4 : Wednesday
+					X
+					X
+		61 5 : Thursday
+		62 6 : Friday
+		63 0 : Saturday
+		64 1 : Sunday
+		65 2 : Monday
+		66 3 : Tuesday
+		67 4 : Wednesday
+		68 5 : Thursday
+		69 6 : Friday
+		70 0 : Saturday
+		71 1 : Sunday
+		72 2 : Monday
+					X
+		73 3 : Tuesday
+		74 4 : Wednesday
+		75 5 : Thursday
+					X
+					X
+		76 6 : Friday
+		77 0 : Saturday
+		78 1 : Sunday
+		79 2 : Monday
+		80 3 : Tuesday
+					X
+		81 4 : Wednesday
+		82 5 : Thursday
+		83 6 : Friday
+		84 0 : Saturday
+					X
+		85 1 : Sunday
+		86 2 : Monday
+		87 3 : Tuesday
+		88 4 : Wednesday
+		89 5 : Thursday
+		90 6 : Friday
+		91 0 : Saturday
+		92 1 : Sunday
+		93 2 : Monday
+		94 3 : Tuesday
+		95 4 : Wednesday
+		96 5 : Thursday
+					X
+		97 6 : Friday
+		98 0 : Saturday
+		99 1 : Sunday
+
+	Days lost: 15
+
+
+
+Process finished with exit code 0
+
+"""
