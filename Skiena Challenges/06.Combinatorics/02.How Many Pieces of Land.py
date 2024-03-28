6292e675df51c810487fdf0f3084b305dc0b4776
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-03-28 23:34:18 +0100

    Skiena 06.02.py v01.0

---
+++ Skiena Challenges/06.Combinatorics/02.How Many Pieces of Land.py
@@ -0,0 +1,29 @@
+"""
+
+How Many Pieces of Land ?
+
+    Inputs:
+        The first line of the input file contains one integer s (0 < s < 3 500), which indicates
+        how many input instances there are. The next s lines describe s input instances, each
+        consisting of exactly one integer n (0 ≤ n < 2^(31)).
+
+    Output:
+        For each input instance output the maximum possible number pieces of land defined
+        by n points, each printed on its own line.
+
+
+    Sample:
+
+        1
+            => 1
+
+        2
+            => 2
+
+        3
+            => 3
+
+        4
+            => 8
+
+"""
