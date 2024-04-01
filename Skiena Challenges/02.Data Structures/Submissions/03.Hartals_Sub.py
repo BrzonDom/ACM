d4cccef023a7d46d03d12c0c632858cece59b869
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-04-01 08:33:54 +0200

    Skiena 02.03.py Sub v02.1

--- Skiena Challenges/02.Data Structures/Submissions/03.Hartals_Sub.py
+++ Skiena Challenges/02.Data Structures/Submissions/03.Hartals_Sub.py
@@ -12,6 +12,7 @@ days = {
     # 7 : "Saturday"
 }
 
+"""
 cases = int(stdin.readline())
 
 for case in range(cases):
@@ -25,8 +26,25 @@ for case in range(cases):
 
     for _ in range(partNum):
         hartLst.append(int(stdin.readline()))
+# """
+
+casesTst = [[14, 3, 3, 4, 8], [100, 4, 12, 15, 25, 40]]
+
+for case in casesTst:
+
+    dayNum = case[0]
+
+    partNum = case[1]
+
+    hartLst = case[2:]
+
+
+    print(f"\tDays: {dayNum}")
+    print(f"\tHartals: {hartLst}")
+    print()
 
     lostDays = 0
+    hartState = copy.deepcopy(hartLst)
 
     for d in range(1, dayNum):
         print(f"\t\t{d:2} {d % 7} : {days[(d % 7)]}")
@@ -35,8 +53,8 @@ for case in range(cases):
 
         for h, hart in enumerate(hartLst):
 
-            if d == hartLst[h]:
-                hartLst[h] += hart
+            if d == hartState[h]:
+                hartState[h] += hart
 
                 if not ((d % 7) == 6 or (d % 7) == 7) and not lost:
                     print("\t\t\t\t\tX")
