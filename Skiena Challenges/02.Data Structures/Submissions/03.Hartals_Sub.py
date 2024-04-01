19eb77bee2baddcaec4f56a22a38edc03e7fd654
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-04-01 21:27:39 +0200

    Skiena 02.03.py Sub v03.1

--- Skiena Challenges/02.Data Structures/Submissions/03.Hartals_Sub.py
+++ Skiena Challenges/02.Data Structures/Submissions/03.Hartals_Sub.py
@@ -12,64 +12,66 @@ days = {
     # 7 : "Saturday"
 }
 
-# # """
-# cases = int(stdin.readline())
-#
-# for case in range(cases):
-#
-#     dayNum = int(stdin.readline())
-#
-#     partNum = int(stdin.readline())
-#
-#     hartLst = []
-#
-#     for _ in range(partNum):
-#         hartLst.append(int(stdin.readline()))
-# # """
-#
-#     """
-#     casesTst = [[14, 3, 3, 4, 8], [100, 4, 12, 15, 25, 40]]
-#
-#     for case in casesTst:
-#
-#         dayNum = case[0]
-#
-#         partNum = case[1]
-#
-#         hartLst = case[2:]
-#     # """
-#
-#     # print(f"\tDays: {dayNum}")
-#     # print(f"\tHartals: {hartLst}")
-#     # print()
-#
-#     lostDays = 0
-#     hartState = copy.deepcopy(hartLst)
-#
-#     for d in range(1, dayNum):
-#         # print(f"\t\t{d:2} {d % 7} : {days[(d % 7)]}")
-#
-#         lost = False
-#
-#         for h, hart in enumerate(hartLst):
-#
-#             if d == hartState[h]:
-#                 hartState[h] += hart
-#
-#                 if not ((d % 7) == 6 or (d % 7) == 7) and not lost:
-#                     # print("\t\t\t\t\tX")
-#                     lostDays += 1
-#                     lost = True
-#
-#                 # elif lost:
-#                     # print("\t\t\t\t\tX")
-#
-#
-#     # print(f"\n\tDays lost: {lostDays}")
-#     # print("\n")
-#
-#     print(lostDays)
+"""
+cases = int(stdin.readline())
 
+for case in range(cases):
+
+    dayNum = int(stdin.readline())
+
+    partNum = int(stdin.readline())
+
+    hartLst = []
+
+    for _ in range(partNum):
+        hartLst.append(int(stdin.readline()))
+
+
+
+
+    # print(f"\tDays: {dayNum}")
+    # print(f"\tHartals: {hartLst}")
+    # print()
+
+    lostDays = 0
+    hartState = copy.deepcopy(hartLst)
+
+    for d in range(1, dayNum):
+        # print(f"\t\t{d:2} {d % 7} : {days[(d % 7)]}")
+
+        lost = False
+
+        for h, hart in enumerate(hartLst):
+
+            if d == hartState[h]:
+                hartState[h] += hart
+
+                if not ((d % 7) == 6 or (d % 7) == 7) and not lost:
+                    # print("\t\t\t\t\tX")
+                    lostDays += 1
+                    lost = True
+
+                # elif lost:
+                    # print("\t\t\t\t\tX")
+
+
+    # print(f"\n\tDays lost: {lostDays}")
+    # print("\n")
+
+    print(lostDays)
+# """
+
+"""
+casesTst = [[14, 3, 3, 4, 8], [100, 4, 12, 15, 25, 40]]
+
+for case in casesTst:
+
+    dayNum = case[0]
+
+    partNum = case[1]
+
+    hartLst = case[2:]
+# """
 
 cases = int(stdin.readline())
 
@@ -92,13 +94,14 @@ for c in range(cases):
 
         while curHart <= dayNum:
 
-            calendar[curHart - 1] = 1
+            if not ((curHart-1) % 7 == 6 or (curHart-1) % 7 == 5):
+                calendar[curHart - 1] = 1
             curHart += hart
 
-    for holDay in range(dayNum):
-
-        if holDay % 7 == 6 or holDay % 7 == 5:
-            calendar[holDay] = 0
+    # for holDay in range(dayNum):
+    #
+    #     if holDay % 7 == 6 or holDay % 7 == 5:
+    #         calendar[holDay] = 0
 
     lostDays = sum(calendar)
 
