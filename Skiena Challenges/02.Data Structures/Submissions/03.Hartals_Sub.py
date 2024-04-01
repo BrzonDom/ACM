73a2956a864a2d43f58b10f2933c3ab7477ce165
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-04-01 08:22:47 +0200

    Skiena 02.03.py Sub v02.0

--- Skiena Challenges/02.Data Structures/Submissions/03.Hartals_Sub.py
+++ Skiena Challenges/02.Data Structures/Submissions/03.Hartals_Sub.py
@@ -1,7 +1,8 @@
 import copy
+from sys import stdin
 
 days = {
-    0: "Saturday",
+    0 : "Saturday",
     1 : "Sunday",
     2 : "Monday",
     3 : "Tuesday",
@@ -11,61 +12,31 @@ days = {
     # 7 : "Saturday"
 }
 
-# days = {
-#     0 : "Sunday",
-#     1 : "Monday",
-#     2 : "Tuesday",
-#     3 : "Wednesday",
-#     4 : "Thursday",
-#     5 : "Friday",
-#     6 : "Saturday"
-# }
+cases = int(stdin.readline())
+
+for case in range(cases):
+
+    dayNum = int(stdin.readline())
+
+    partNum = int(stdin.readline())
+
+    hartLst = []
 
 
-Input_Str = "2 14 3 3 4 8 100 4 12 15 25 40"
-
-Input = list(map(int, Input_Str.split()))
-
-print(f"Input string:   \"{Input_Str}\"")
-print(f"Input:         {Input}")
-print("\n")
-
-Input_ext = copy.deepcopy(Input)
-
-Cases = [[] for _ in range(Input[0])]
-
-Input_ext = Input_ext[1:]
-
-# print(Input_ext)
-
-for c in range(Input[0]):
-
-    Cases[c].append(Input_ext[0])
-    Cases[c].append([])
-    Input_ext = Input_ext[1:]
-
-    for p in range(1, Input_ext[0] + 1):
-        Cases[c][1].append(Input_ext[p])
-
-    Input_ext = Input_ext[Input_ext[0] + 1:]
-
-    print(f"\tDays: {Cases[c][0]}")
-    print(f"\tCases: {Cases[c][1]}")
-    print()
+    for _ in range(partNum):
+        hartLst.append(int(stdin.readline()))
 
     lostDays = 0
 
-    Hartals = copy.deepcopy(Cases[c][1])
-
-    for d in range(1, Cases[c][0]):
+    for d in range(1, dayNum):
         print(f"\t\t{d:2} {d % 7} : {days[(d % 7)]}")
 
         lost = False
 
-        for cs, case in enumerate(Cases[c][1]):
+        for h, hart in enumerate(hartLst):
 
-            if d == Hartals[cs]:
-                Hartals[cs] += case
+            if d == hartLst[h]:
+                hartLst[h] += hart
 
                 if not ((d % 7) == 6 or (d % 7) == 7) and not lost:
                     print("\t\t\t\t\tX")
