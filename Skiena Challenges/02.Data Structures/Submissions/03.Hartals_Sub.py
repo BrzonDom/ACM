c228dfe2526fbbe64ac17d757231205a6bd20fdc
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-04-01 21:24:08 +0200

    Skiena 02.03.py Sub v02.2

--- Skiena Challenges/02.Data Structures/Submissions/03.Hartals_Sub.py
+++ Skiena Challenges/02.Data Structures/Submissions/03.Hartals_Sub.py
@@ -12,7 +12,7 @@ days = {
     # 7 : "Saturday"
 }
 
-"""
+# """
 cases = int(stdin.readline())
 
 for case in range(cases):
@@ -23,31 +23,31 @@ for case in range(cases):
 
     hartLst = []
 
-
     for _ in range(partNum):
         hartLst.append(int(stdin.readline()))
 # """
 
-casesTst = [[14, 3, 3, 4, 8], [100, 4, 12, 15, 25, 40]]
+    """
+    casesTst = [[14, 3, 3, 4, 8], [100, 4, 12, 15, 25, 40]]
+    
+    for case in casesTst:
+    
+        dayNum = case[0]
+    
+        partNum = case[1]
+    
+        hartLst = case[2:]
+    # """
 
-for case in casesTst:
-
-    dayNum = case[0]
-
-    partNum = case[1]
-
-    hartLst = case[2:]
-
-
-    print(f"\tDays: {dayNum}")
-    print(f"\tHartals: {hartLst}")
-    print()
+    # print(f"\tDays: {dayNum}")
+    # print(f"\tHartals: {hartLst}")
+    # print()
 
     lostDays = 0
     hartState = copy.deepcopy(hartLst)
 
     for d in range(1, dayNum):
-        print(f"\t\t{d:2} {d % 7} : {days[(d % 7)]}")
+        # print(f"\t\t{d:2} {d % 7} : {days[(d % 7)]}")
 
         lost = False
 
@@ -57,13 +57,16 @@ for case in casesTst:
                 hartState[h] += hart
 
                 if not ((d % 7) == 6 or (d % 7) == 7) and not lost:
-                    print("\t\t\t\t\tX")
+                    # print("\t\t\t\t\tX")
                     lostDays += 1
                     lost = True
 
-                elif lost:
-                    print("\t\t\t\t\tX")
+                # elif lost:
+                    # print("\t\t\t\t\tX")
 
 
-    print(f"\n\tDays lost: {lostDays}")
-    print("\n")
+    # print(f"\n\tDays lost: {lostDays}")
+    # print("\n")
+
+    print(lostDays)
+
