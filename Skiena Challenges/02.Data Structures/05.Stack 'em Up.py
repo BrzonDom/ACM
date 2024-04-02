6b194cb304bb0841f82678508c5647c1c3688130
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-04-03 01:59:24 +0200

    Skiena 02.05.py v03.1

--- Skiena Challenges/02.Data Structures/05.Stack 'em Up.py
+++ Skiena Challenges/02.Data Structures/05.Stack 'em Up.py
@@ -129,12 +129,37 @@ for case in range(caseNum):
         print(f"\t\t\t{shffl+1}.:\t{shfflLst[shffl]}")
     print()
 
-    usedLst = []
+    moveLst = []
 
     while InputStr_Lst[0] != '':
-        usedLst.append(int(InputStr_Lst.pop(0)))
+        moveLst.append(int(InputStr_Lst.pop(0)))
 
-    print(f"\t\tUsed shuffles: {usedLst}")
+    print(f"\t\tUsed shuffles: {moveLst}\n")
+
+    prev_Cards = ['2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC', 'AC',
+                  '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '10D', 'JD', 'QD', 'KD', 'AD',
+                  '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JH', 'QH', 'KH', 'AH',
+                  '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', 'JS', 'QS', 'KS', 'AS']
+
+    next_Cards = copy.deepcopy(prev_Cards)
+
+    for move in moveLst:
+
+        for to, frm in enumerate(shfflLst[move - 1]):
+
+            next_Cards[to] = prev_Cards[frm-1]
+
+        prev_Cards = copy.deepcopy(next_Cards)
+
+    print(f"\t\tShuffled cards:", end="\n\n\t\t\t")
+
+    for c, card in enumerate(next_Cards):
+
+        if (c+1) % 13 == 0:
+            print(card, end="\n\t\t\t")
+
+        else:
+            print(card, end=" ")
 
     print("\n")
 
@@ -312,6 +337,13 @@ Num. of cases: 2
 
 		Used shuffles: [1, 2]
 
+		Shuffled cards:
+
+			KS 2C 4C 5C 6C 7C 8C 9C 10C JC QC KC AC
+			2D 3D 4D 5D 6D 7D 8D 9D 10D JD QD KD AD
+			2H 3H 4H 5H 6H 7H 8H 9H 10H JH QH KH AH
+			2S 3S 4S 5S 6S 7S 8S 9S 10S JS QS AS 3C
+			
 
 	2.Case:
 
@@ -321,6 +353,13 @@ Num. of cases: 2
 
 		Used shuffles: [1]
 
+		Shuffled cards:
+
+			3C 2C 4C 5C 6C 7C 8C 9C 10C JC QC KC AC
+			2D 3D 4D 5D 6D 7D 8D 9D 10D JD QD KD AD
+			2H 3H 4H 5H 6H 7H 8H 9H 10H JH QH KH AH
+			2S 3S 4S 5S 6S 7S 8S 9S 10S JS QS AS KS
+			
 
 ['']
 
