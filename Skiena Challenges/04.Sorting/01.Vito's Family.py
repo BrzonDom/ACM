e994698efe245aae2671a16ce5f4cab568ecf95c
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-04-06 23:49:32 +0200

    Skiena 04.01.py v03.0

--- Skiena Challenges/04.Sorting/01.Vito's Family.py
+++ Skiena Challenges/04.Sorting/01.Vito's Family.py
@@ -36,67 +36,69 @@ InputRaw_Str = """
 3 2 4 6
 """
 
-Cases = int(InputRaw_Str[1])
+if __name__ == "__main__":
 
-for case in range(Cases):
-    Input_Str = InputRaw_Str.split("\n")[case+2]
-    Input = list(map(int, Input_Str.split()))
+    Cases = int(InputRaw_Str[1])
 
-    print(f"Input: {Input}\n")
+    for case in range(Cases):
+        Input_Str = InputRaw_Str.split("\n")[case+2]
+        Input = list(map(int, Input_Str.split()))
 
-    RelatNum = Input[0]
-    HousLst = Input[1:]
+        print(f"Input: {Input}\n")
 
-    print(f"\tRelatives: {RelatNum}")
-    print(f"\tHouse numbers: {HousLst}")
-    print()
+        RelatNum = Input[0]
+        HousLst = Input[1:]
 
-    HousLst = sorted(HousLst)
-
-    if RelatNum % 2 == 0:
-        Med = HousLst[RelatNum // 2 - 1] + HousLst[RelatNum // 2]
-
-        if Med % 2 != 0:
-            print(f"\t\tMedian:  {Med / 2}")
-            print(f"\t\t\tRounded:       {round(Med / 2)}")
-            print(f"\t\t\tRounded up:    {Med // 2 + 1}")
-            print(f"\t\t\tRounded down:  {Med // 2}")
-
-            Median = round(Med / 2)
-            # MedLst = [Median - 1, Median, Median + 1]
-
-        else:
-            print(f"\t\tMedian:  {Med // 2}")
-
-            Median = Med // 2
-            # MedLst = [Median - 1, Median, Median + 1]
-
-    else:
-        Median = HousLst[(RelatNum - 1) // 2]
-
-        print(f"\t\tMedian:  {Median}")
-    print()
-
-    Sum = 0
-
-    for hous in HousLst:
-        Sum += abs(hous - Median)
-
-    print(f"\t\tSum:  {Sum}")
-    print()
-
-    print(f"\t\tTest Medians:")
-
-    MedLst = [Median - 1, Median, Median + 1]
-
-    for Med in MedLst:
-        print(f"\t\t\tMedian:  {Med}")
-        SumTst = 0
-
-        for hous in HousLst:
-            SumTst += abs(hous - Med)
-
-        print(f"\t\t\t\tSum:  {Sum}")
+        print(f"\tRelatives: {RelatNum}")
+        print(f"\tHouse numbers: {HousLst}")
         print()
 
-    print("\n")
+        HousLst = sorted(HousLst)
+
+        if RelatNum % 2 == 0:
+            Med = HousLst[RelatNum // 2 - 1] + HousLst[RelatNum // 2]
+
+            if Med % 2 != 0:
+                print(f"\t\tMedian:  {Med / 2}")
+                print(f"\t\t\tRounded:       {round(Med / 2)}")
+                print(f"\t\t\tRounded up:    {Med // 2 + 1}")
+                print(f"\t\t\tRounded down:  {Med // 2}")
+
+                Median = round(Med / 2)
+                # MedLst = [Median - 1, Median, Median + 1]
+
+            else:
+                print(f"\t\tMedian:  {Med // 2}")
+
+                Median = Med // 2
+                # MedLst = [Median - 1, Median, Median + 1]
+
+        else:
+            Median = HousLst[(RelatNum - 1) // 2]
+
+            print(f"\t\tMedian:  {Median}")
+        print()
+
+        Sum = 0
+
+        for hous in HousLst:
+            Sum += abs(hous - Median)
+
+        print(f"\t\tSum:  {Sum}")
+        print()
+
+        print(f"\t\tTest Medians:")
+
+        MedLst = [Median - 1, Median, Median + 1]
+
+        for Med in MedLst:
+            print(f"\t\t\tMedian:  {Med}")
+            SumTst = 0
+
+            for hous in HousLst:
+                SumTst += abs(hous - Med)
+
+            print(f"\t\t\t\tSum:  {Sum}")
+            print()
+
+        print("\n")
