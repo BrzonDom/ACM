09841e38dded4fcf78f7a5c7c2b55767bce4ac45
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-04-09 21:53:51 +0200

    Skiena 03.03.py v01.0

---
+++ Skiena Challenges/03.Strings/03.Common Permutation.py
@@ -0,0 +1,31 @@
+"""
+
+Common Permutation
+
+        Given two strings a and b, print the longest string x of letters such that there is a
+    permutation of x that is a subsequence of a and there is a permutation of x that is a
+    subsequence of b.
+
+    Input:
+            The input file contains several cases, each case consisting of two consecutive lines. This
+        means that lines 1 and 2 are a test case, lines 3 and 4 are another test case, and so on.
+        Each line contains one string of lowercase characters, with first line of a pair denoting
+        a and the second denoting b. Each string consists of at most 1,000 characters
+
+    Output:
+            For each set of input, output a line containing x. If several x satisfy the criteria above,
+        choose the first one in alphabetical order
+
+
+    Sample:
+        pretty
+        women
+        walking
+        down
+        the
+        street
+            =>  e
+                nw
+                et
+
+"""
