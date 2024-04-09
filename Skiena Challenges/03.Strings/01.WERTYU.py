1be30844190f25a93c00b6101fd3e2beb5184e64
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-04-09 13:46:21 +0200

    Skiena 03.01.py v01.0

---
+++ Skiena Challenges/03.Strings/01.WERTYU.py
@@ -0,0 +1,20 @@
+"""
+
+WERTYU
+
+    Input:
+        Input consists of several lines of text. Each line may contain digits, spaces, uppercase
+        letters (except “Q”, “A”, “Z”), or punctuation shown above [except back-quote (‘)].
+        Keys labeled with words [Tab, BackSp, Control, etc.] are not represented in the input.
+
+    Output:
+        You are to replace each letter or punctuation symbol by the one immediately to its left
+        on the QWERTY keyboard shown above. Spaces in the input should be echoed in the
+        output.
+
+
+    Sample:
+        O S, GOMR YPFSU/
+            =>  I AM FINE TODAY.
+
+"""
