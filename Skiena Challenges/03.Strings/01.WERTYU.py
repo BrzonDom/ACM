3250178fe72a0016c08812ce62e0a379357d1893
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-04-09 14:08:49 +0200

    Skiena 03.01.py v01.2

--- Skiena Challenges/03.Strings/01.WERTYU.py
+++ Skiena Challenges/03.Strings/01.WERTYU.py
@@ -21,5 +21,25 @@ WERTYU
 
 InputRaw_Str = "O S, GOMR YPFSU/"
 
+decodeDict = {'1':'`', '2':'1', '3':'2', '4':'3', '5':'4', '6':'5',
+              '7':'6', '8':'7', '9':'8', '0':'9', '-':'0', '=':'-',
+
+              'W':'Q', 'E':'W', 'R':'E', 'T':'R', 'Y':'T', 'U':'Y',
+              'I':'U', 'O':'I', 'P':'O', '[':'P', ']':'[', '\\':']'
+              }
+
+
 print(f"Input: {InputRaw_Str}")
 print()
+
+row1 = "`1234567890-="
+row2 = "QWERTYUIOP[]\\"
+row3 = "ASDFGHJKL;'"
+row4 = "ZXCVBNM,./"
+
+rows = [row1, row2, row3, row4]
+
+for row in rows:
+    for c, char in enumerate(row[1:]):
+        print(f"\'{char}\' : \'{row[c]}\'", end=", ")
+    print()
