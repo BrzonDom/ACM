b7ee7efd96c8be19cc3239cc2549df53bf1af756
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-04-11 14:11:10 +0200

    01.Cv E 03.py v01.0

---
+++ 01.Cv/Easy/03.10038 - Jolly Jumpers.py
@@ -0,0 +1,24 @@
+"""
+10038 - Jolly Jumpers
+https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=30&page=show_problem&problem=979
+
+    A sequence of n > 0 integers is called a jolly jumper if the absolute values of the
+    differences between successive elements take on all possible values 1 through n − 1.
+
+
+    Inputs:
+        Each line of input contains an integer n < 3, 000 followed by n integers representing the
+        sequence.
+
+    Output:
+        For each line of input generate a line of output saying “Jolly” or “Not jolly”.
+
+
+    Sample:
+
+        4 1 4 2 3
+        5 1 4 2 -1 6
+            =>  Not Jolly
+                Jolly
+
+"""
