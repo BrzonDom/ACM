426aa63b4e930f723f400b59b9065d5521d9084d
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-04-16 22:55:41 +0200

    01.Cv E 07.py v03.5

--- 01.Cv/Easy/07.GERGOVIA - Wine trading in Gergovia.py
+++ 01.Cv/Easy/07.GERGOVIA - Wine trading in Gergovia.py
@@ -77,6 +77,47 @@ def sumHsPrnt(BuySell):
     print()
 
 
+def mvsHsPrnt(BuySell):
+
+    cntMvs = 0
+
+    while not all(hs == 0 for hs in BuySell):
+
+        for h, hs in enumerate(BuySell):
+
+            if hs > 0:
+
+                lftHs = sum(BuySell[:h])
+                rgtHs = sum(BuySell[h + 1:])
+
+                if lftHs > rgtHs:
+                    move = min(-rgtHs, hs)
+
+                    BuySell[h] -= move
+                    BuySell[h + 1] += move
+                    cntMvs += move
+
+                elif lftHs < rgtHs:
+                    move = min(-lftHs, hs)
+
+                    BuySell[h] -= move
+                    BuySell[h - 1] += move
+                    cntMvs += move
+
+                else:
+                    move = hs // 2
+
+                    BuySell[h] -= 2 * move
+                    BuySell[h - 1] += move
+                    BuySell[h + 1] += move
+                    cntMvs += 2 * move
+
+                print(f"\t\t{BuySell}")
+        print()
+
+    return cntMvs
+
+
 if __name__ == "__main__":
 
     print("Input:")
@@ -125,41 +166,43 @@ if __name__ == "__main__":
         #         print("<> Move both")
         # print()
 
-        cntMvs = 0
+        cntMvs = mvsHsPrnt(BuySell)
 
-        while not all(hs == 0 for hs in BuySell):
-
-            for h, hs in enumerate(BuySell):
-
-                if hs > 0:
-
-                    lftHs = sum(BuySell[:h])
-                    rgtHs = sum(BuySell[h+1:])
-
-                    if lftHs > rgtHs:
-                        move = min(-rgtHs, hs)
-
-                        BuySell[h] -= move
-                        BuySell[h+1] += move
-                        cntMvs += move
-
-                    elif lftHs < rgtHs:
-                        move = min(-lftHs, hs)
-
-                        BuySell[h] -= move
-                        BuySell[h-1] += move
-                        cntMvs += move
-
-                    else:
-                        move = hs // 2
-
-                        BuySell[h] -= 2 * move
-                        BuySell[h-1] += move
-                        BuySell[h+1] += move
-                        cntMvs += 2 * move
-
-                    print(f"\t\t{BuySell}")
-            print()
+        # cntMvs = 0
+        #
+        # while not all(hs == 0 for hs in BuySell):
+        #
+        #     for h, hs in enumerate(BuySell):
+        #
+        #         if hs > 0:
+        #
+        #             lftHs = sum(BuySell[:h])
+        #             rgtHs = sum(BuySell[h+1:])
+        #
+        #             if lftHs > rgtHs:
+        #                 move = min(-rgtHs, hs)
+        #
+        #                 BuySell[h] -= move
+        #                 BuySell[h+1] += move
+        #                 cntMvs += move
+        #
+        #             elif lftHs < rgtHs:
+        #                 move = min(-lftHs, hs)
+        #
+        #                 BuySell[h] -= move
+        #                 BuySell[h-1] += move
+        #                 cntMvs += move
+        #
+        #             else:
+        #                 move = hs // 2
+        #
+        #                 BuySell[h] -= 2 * move
+        #                 BuySell[h-1] += move
+        #                 BuySell[h+1] += move
+        #                 cntMvs += 2 * move
+        #
+        #             print(f"\t\t{BuySell}")
+        #     print()
 
         print(f"\t\tNeeded moves: {cntMvs}")
 
