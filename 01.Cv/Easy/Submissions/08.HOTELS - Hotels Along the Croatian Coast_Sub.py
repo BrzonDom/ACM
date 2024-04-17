f65eb60c021c4ad605b1a34b99c35373b1e9b0f2
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-04-17 04:46:34 +0200

    01.Cv E 08.py Sub v02.0

--- 01.Cv/Easy/Submissions/08.HOTELS - Hotels Along the Croatian Coast_Sub.py
+++ 01.Cv/Easy/Submissions/08.HOTELS - Hotels Along the Croatian Coast_Sub.py
@@ -82,73 +82,19 @@ if __name__ == "__main__":
 
     InputStr_Lst = InputRaw_Str.split("\n")
 
-    caseCnt = 1
+    # for lineInf, lineHtls in zip(InputStr_Lst[::2], InputStr_Lst[1::2]):
 
-    for lineInf, lineHtls in zip(InputStr_Lst[::2], InputStr_Lst[1::2]):
+        # print(f"\t{caseCnt}. Case")
+        # print()
 
-        print(f"\t{caseCnt}. Case")
-        print()
+    lineInf = input()
+    lineHtls = input()
 
-        caseCnt += 1
+    htlNum, mnyNum, Htls = infExtrc(lineInf, lineHtls)
 
-        htlNum, mnyNum, Htls = infExtrc(lineInf, lineHtls)
+    mnyMax, htlMax = fndMaxMny(Htls, mnyNum)
 
-        mnyMax, htlMax = fndMaxMny(Htls, mnyNum)
+    print(f"\t\tMax money: {mnyMax}")
+    print(f"\t\tMax hotels: {htlMax}")
 
-        print(f"\t\tMax money: {mnyMax}")
-        print(f"\t\tMax hotels: {htlMax}")
-
-        print("\n")
-
-
-"""__Output__"""
-"""
-Input:
-5 12
-2 1 3 4 5
-4 9
-7 3 5 6
-
-	1. Case
-
-		Num. of hotels: 5
-		Num. of money:  12
-
-		Hotels: [2, 1, 3, 4, 5]
-
-			[2] (2)
-			[2, 1] (3)
-			[2, 1, 3] (6)
-			[2, 1, 3, 4] (10)
-			[1] (1)
-			[1, 3] (4)
-			[1, 3, 4] (8)
-			[3] (3)
-			[3, 4] (7)
-			[3, 4, 5] (12)
-
-		Max money: 12
-		Max hotels: [3, 4, 5]
-
-
-	2. Case
-
-		Num. of hotels: 4
-		Num. of money:  9
-
-		Hotels: [7, 3, 5, 6]
-
-			[7] (7)
-			[3] (3)
-			[3, 5] (8)
-			[5] (5)
-			[6] (6)
-
-		Max money: 8
-		Max hotels: [3, 5]
-
-
-
-Process finished with exit code 0
-
-"""
+    print("\n")
