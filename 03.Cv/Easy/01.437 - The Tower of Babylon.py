96fe5beff09c6bf3745cc3b37ae68f94f8dfe87b
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-05-07 20:12:13 +0200

    03.Cv E 01.py v01.1

--- 03.Cv/Easy/01.437 - The Tower of Babylon.py
+++ 03.Cv/Easy/01.437 - The Tower of Babylon.py
@@ -58,3 +58,31 @@ https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=6&pag
                 Case 4: maximum height = 342
 
 """
+
+InputRaw_Str = """
+1
+10 20 30
+2
+6 8 10
+5 5 5
+7
+1 1 1
+2 2 2
+3 3 3
+4 4 4
+5 5 5
+6 6 6
+7 7 7
+5
+31 41 59
+26 53 58
+97 93 23
+84 62 64
+33 83 27
+0
+"""
+
+InputRaw_Str = InputRaw_Str[1:-1]
+
+print("Input:")
+print(InputRaw_Str)
