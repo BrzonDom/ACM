05f3d7fbc31e8eb10cc7507f70ee2655bfcc8e5a
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-05-12 23:46:19 +0200

    07.Cv E 02.py v01.1

--- 07.Cv/Easy/02.993 - Product of digits.py
+++ 07.Cv/Easy/02.993 - Product of digits.py
@@ -2,16 +2,15 @@
 993 - Product of digits
 https://onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=934
 
-        For a given non-negative integer number N , find the minimal natural Q such that the product of all
-    digits of Q is equal N .
+        For a given non-negative integer number N, find the minimal natural Q such that the product of all
+    digits of Q is equal N.
 
     Input:
             The first line of input contains one positive integer number, which is the number of data sets. Each
-        subsequent line contains one data set which consists of one non-negative integer number N (0 ≤ N ≤
-        109).
+        subsequent line contains one data set which consists of one non-negative integer number N (0 ≤ N ≤ 10^9).
 
     Output:
-            For each data set, write one line containing the corresponding natural number Q or ‘-1’ if Q does not
+            For each data set, write one line containing the corresponding natural number Q or '-1' if Q does not
         exist.
 
 
