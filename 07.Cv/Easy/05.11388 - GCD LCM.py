354413d45226a71ecebbee2360c079e1b22be894
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-05-14 23:09:01 +0200

    07.Cv E 05.py v02.0

--- 07.Cv/Easy/05.11388 - GCD LCM.py
+++ 07.Cv/Easy/05.11388 - GCD LCM.py
@@ -60,3 +60,28 @@ for cs in range(caseNum):
     print(f"\t\t\t1.Num: {nums[0]}")
     print(f"\t\t\t2.Num: {nums[1]}")
     print()
+
+
+"""__Output__"""
+"""
+Input:
+2
+1 2
+3 4
+
+	Cases: 2
+
+		1. Case
+
+			1.Num: 1
+			2.Num: 2
+
+		2. Case
+
+			1.Num: 3
+			2.Num: 4
+
+
+Process finished with exit code 0
+
+"""
