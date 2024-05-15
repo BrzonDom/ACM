df0d5a32a828124ba73d6d99e1ef242783b0283a
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-05-15 17:00:20 +0200

    07.Cv E 01.py Sub v01.1

--- 07.Cv/Easy/Submissions/01.543 - Goldbach's Conjecture_Sub.py
+++ 07.Cv/Easy/Submissions/01.543 - Goldbach's Conjecture_Sub.py
@@ -1382,24 +1382,6 @@ def fndPrimeTable(num):
     return PrmTable
 
 
-"""
-tstLst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-print(tstLst)
-print(len(tstLst))
-print()
-
-if len(tstLst) % 2 == 0:
-    midTst = len(tstLst) // 2
-else:
-    midTst = len(tstLst) // 2 + 1
-
-print(midTst)
-print(tstLst[:midTst], tstLst[midTst:])
-
-quit()
-"""
-
 if __name__ == '__main__':
 
     print("Input:")
