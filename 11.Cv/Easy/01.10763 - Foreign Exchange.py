96430f356f2ee81972e669abaab25656a85f2313
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-05-20 20:29:08 +0200

    11.Cv E 01.py v03.6

--- 11.Cv/Easy/01.10763 - Foreign Exchange.py
+++ 11.Cv/Easy/01.10763 - Foreign Exchange.py
@@ -188,36 +188,6 @@ def inDataRead(InLines, candNum):
     return Candid
 
 
-def inDataAllPrint(AllCandid, caseNum):
-
-    print(f"\tCases: {caseNum}")
-    print()
-
-    for caseCnt, Candid in enumerate(AllCandid):
-
-        print(f"\t\t{caseCnt+1}. Case")
-        print()
-
-        candNum = len(Candid)
-        print(f"\t\t\tCandid. num.: {candNum}")
-        print()
-
-        for cnd, cand in enumerate(Candid):
-            print(f"\t\t\t\t{cnd+1:2}. {cand}")
-
-        if (caseCnt+1) < caseNum:
-            print("\n")
-
-
-def inDataPrint(Candid, candNum):
-
-    print(f"\t\t\tCandid. num.: {candNum}")
-    print()
-
-    for cnd, cand in enumerate(Candid):
-        print(f"\t\t\t\t{cnd + 1:2}. {cand}")
-
-
 def inDataAllExtract(InputRaw):
     """
             Reads all the input data and sorts them
@@ -278,6 +248,90 @@ def inDataAllExtract(InputRaw):
     return AllCandid, caseCnt, AllDataFr, AllDataTo
 
 
+def inDataExtract(InLines, candNum):
+    """
+            Reads input data for a case and sorts
+        them into a list (Candid)
+            While extracting data into dictionaries,
+        a dictionary From (DataFr)
+        and a dictionary to (DataTo)
+    """
+
+    Candid = []
+
+    DataFr = {}
+    DataTo = {}
+
+    for cnd in range(candNum):
+        cand = (list(map(int, InLines.pop(0).split())))
+
+        Candid.append(cand)
+
+        fr, to = cand[0], cand[1]
+
+        if fr in DataFr:
+            DataFr[fr] += 1
+        else:
+            DataFr[fr] = 1
+
+        if to in DataTo:
+            DataTo[to] += 1
+        else:
+            DataTo[to] = 1
+
+    return Candid, DataFr, DataTo
+
+
+def inDataAllPrint(AllCandid, caseNum):
+
+    print(f"\tCases: {caseNum}")
+    print()
+
+    for caseCnt, Candid in enumerate(AllCandid):
+
+        print(f"\t\t{caseCnt+1}. Case")
+        print()
+
+        candNum = len(Candid)
+        print(f"\t\t\tCandid. num.: {candNum}")
+        print()
+
+        for cnd, cand in enumerate(Candid):
+            print(f"\t\t\t\t{cnd+1:2}. {cand}")
+
+        if (caseCnt+1) < caseNum:
+            print("\n")
+
+
+def inDataPrint(Candid, candNum):
+
+    print(f"\t\t\tCandid. num.: {candNum}")
+    print()
+
+    for cnd, cand in enumerate(Candid):
+        print(f"\t\t\t\t{cnd + 1:2}. {cand}")
+
+
+def extDataPrint(DataFr, DataTo):
+
+    print("\t\t\t\tFrom/To Data:")
+    print()
+
+    FrToKys = set(list(DataFr.keys()) + list(DataTo.keys()))
+
+    for FrTo in FrToKys:
+
+        if FrTo in DataFr:
+            if FrTo in DataTo:
+                print(f"\t\t\t\t\t{FrTo:3}:  {DataFr[FrTo]:2}  |  {DataTo[FrTo]:2}")
+
+            else:
+                print(f"\t\t\t\t\t{FrTo:3}:  {DataFr[FrTo]:2}  |   0")
+
+        else:
+            print(f"\t\t\t\t\t{FrTo:3}:   0  |  {DataTo[FrTo]:2}")
+
+
 if __name__ == '__main__':
 
     print("Input:")
@@ -291,13 +345,17 @@ if __name__ == '__main__':
     while candNum != 0:
         print("\n")
 
-        Candid = inDataRead(InLines, candNum)
+        # Candid = inDataRead(InLines, candNum)
+        Candid, DataFr, DataTo = inDataExtract(InLines, candNum)
 
         print(f"\t\t{caseCnt}. Case")
         print()
         caseCnt += 1
 
         inDataPrint(Candid, candNum)
+        print()
+
+        extDataPrint(DataFr, DataTo)
 
         candNum = int(InLines.pop(0))
 
@@ -407,6 +465,16 @@ Input:
 				 9. [1, 2]
 				10. [2, 1]
 
+				From/To Data:
+
+					  1:   2  |   2
+					  2:   3  |   3
+					  3:   1  |   1
+					100:   1  |   1
+					  4:   1  |   1
+					200:   1  |   1
+					 57:   1  |   1
+
 
 		2. Case
 
@@ -422,6 +490,29 @@ Input:
 				 8. [15, 16]
 				 9. [17, 18]
 				10. [19, 20]
+
+				From/To Data:
+
+					  1:   1  |   0
+					  2:   0  |   1
+					  3:   1  |   0
+					  4:   0  |   1
+					  5:   1  |   0
+					  6:   0  |   1
+					  7:   1  |   0
+					  8:   0  |   1
+					  9:   1  |   0
+					 10:   0  |   1
+					 11:   1  |   0
+					 12:   0  |   1
+					 13:   1  |   0
+					 14:   0  |   1
+					 15:   1  |   0
+					 16:   0  |   1
+					 17:   1  |   0
+					 18:   0  |   1
+					 19:   1  |   0
+					 20:   0  |   1
 
 Process finished with exit code 0
 
