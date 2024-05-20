ee0abe43db2844376e10fb2f98d306a7510afb00
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-05-20 17:56:28 +0200

    11.Cv E 01.py Sub v01.0

---
+++ 11.Cv/Easy/Submissions/01.10763 - Foreign Exchange_Sub.py
@@ -0,0 +1,285 @@
+"""
+10763 - Foreign Exchange
+https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=315&page=show_problem&problem=1704
+
+    Your non-profit organization (iCORE - international Confederation of Revolver Enthusiasts) coor-
+    dinates a very successful foreign student exchange program. Over the last few years, demand has
+    sky-rocketed and now you need assistance with your task.
+    The program your organization runs works as follows: All candidates are asked for their original
+    location and the location they would like to go to. The program works out only if every student has a
+    suitable exchange partner. In other words, if a student wants to go from A to B, there must be another
+    student who wants to go from B to A. This was an easy task when there were only about 50 candidates,
+    however now there are up to 500000 candidates!
+
+    Input:
+            The input file contains multiple cases. Each test case will consist of a line containing n – the number
+        of candidates (1 ≤ n ≤ 500000), followed by n lines representing the exchange information for each
+        candidate. Each of these lines will contain 2 integers, separated by a single space, representing the
+        candidate’s original location and the candidate’s target location respectively. Locations will be repre-
+        sented by nonnegative integer numbers. You may assume that no candidate will have his or her original
+        location being the same as his or her target location as this would fall into the domestic exchange
+        program. The input is terminated by a case where n = 0; this case should not be processed.
+
+    Output:
+            For each test case, print ‘YES’ on a single line if there is a way for the exchange program to work out,
+        otherwise print ‘NO’
+
+    Sample:
+        10
+        1 2
+        2 1
+        3 4
+        4 3
+        100 200
+        200 100
+        57 2
+        2 57
+        1 2
+        2 1
+        10
+        1 2
+        3 4
+        5 6
+        7 8
+        9 10
+        11 12
+        13 14
+        15 16
+        17 18
+        19 20
+        0
+            =>  YES
+                NO
+
+"""
+
+InputOrg_Raw = """
+10
+1 2
+2 1
+3 4
+4 3
+100 200
+200 100
+57 2
+2 57
+1 2
+2 1
+10
+1 2
+3 4
+5 6
+7 8
+9 10
+11 12
+13 14
+15 16
+17 18
+19 20
+0
+"""
+
+InputUVa_Raw = """
+4
+1 3
+2 4
+5 7
+14 8
+3
+1 2
+1 2
+2 1
+0
+"""
+
+InputOrg_Raw = InputOrg_Raw[1:-1]
+InputUVa_Raw = InputUVa_Raw[1:-1]
+
+InputRaw_Lst = [InputOrg_Raw, InputUVa_Raw]
+
+InputRaw = InputRaw_Lst[0]
+
+if __name__ == '__main__':
+
+    print("Input:")
+    print(InputRaw)
+    print()
+
+    InLines = InputRaw.split("\n")
+
+    caseNum = int(InLines.pop(0))
+
+    while caseNum != 0:
+
+        print(f"\tCases: {caseNum}")
+        print()
+
+        Cases = []
+
+        DataTo = {}
+        DataFr = {}
+
+        for cs in range(caseNum):
+            case = (list(map(int, InLines.pop(0).split())))
+            print(f"\t\t{case}")
+
+            fr, to = case[0], case[1]
+
+            Cases.append(case)
+
+            if fr in DataFr:
+                DataFr[fr] += 1
+            else:
+                DataFr[fr] = 1
+
+            if to in DataTo:
+                DataTo[to] += 1
+            else:
+                DataTo[to] = 1
+
+        print()
+
+        print("\t\tFrom/To Data:")
+        print()
+
+        FrToKys = set(list(DataFr.keys()) + list(DataTo.keys()))
+
+        for FrTo in FrToKys:
+
+            if FrTo in DataFr:
+                if FrTo in DataTo:
+                    print(f"\t\t\t{FrTo:3}:  {DataFr[FrTo]:2}  |  {DataTo[FrTo]:2}")
+
+                else:
+                    print(f"\t\t\t{FrTo:3}:  {DataFr[FrTo]:2}  |   0")
+
+            else:
+                print(f"\t\t\t{FrTo:3}:   0  |  {DataTo[FrTo]:2}")
+
+        print()
+
+        NoPlace = False
+
+        for FrKys in DataFr.keys():
+            for Fr in range(DataFr[FrKys]):
+
+                if FrKys in DataTo:
+                    if DataTo[FrKys]:
+                        DataTo[FrKys] -= 1
+
+                    else:
+                        NoPlace = True
+                        break
+
+                else:
+                    NoPlace = True
+                    break
+
+            if NoPlace:
+                break
+
+        if NoPlace:
+            print("\t\tNO, no all exchange places found")
+        else:
+            print("\t\tYES, yes all exchange places found")
+
+        print("\n")
+
+        caseNum = int(InLines.pop(0))
+
+"""__Output__"""
+"""
+Input:
+10
+1 2
+2 1
+3 4
+4 3
+100 200
+200 100
+57 2
+2 57
+1 2
+2 1
+10
+1 2
+3 4
+5 6
+7 8
+9 10
+11 12
+13 14
+15 16
+17 18
+19 20
+0
+
+	Cases: 10
+
+		[1, 2]
+		[2, 1]
+		[3, 4]
+		[4, 3]
+		[100, 200]
+		[200, 100]
+		[57, 2]
+		[2, 57]
+		[1, 2]
+		[2, 1]
+
+		From/To Data:
+
+			  1:   2  |   2
+			  2:   3  |   3
+			  3:   1  |   1
+			100:   1  |   1
+			  4:   1  |   1
+			200:   1  |   1
+			 57:   1  |   1
+
+		YES, yes all exchange places found
+
+
+	Cases: 10
+
+		[1, 2]
+		[3, 4]
+		[5, 6]
+		[7, 8]
+		[9, 10]
+		[11, 12]
+		[13, 14]
+		[15, 16]
+		[17, 18]
+		[19, 20]
+
+		From/To Data:
+
+			  1:   1  |   0
+			  2:   0  |   1
+			  3:   1  |   0
+			  4:   0  |   1
+			  5:   1  |   0
+			  6:   0  |   1
+			  7:   1  |   0
+			  8:   0  |   1
+			  9:   1  |   0
+			 10:   0  |   1
+			 11:   1  |   0
+			 12:   0  |   1
+			 13:   1  |   0
+			 14:   0  |   1
+			 15:   1  |   0
+			 16:   0  |   1
+			 17:   1  |   0
+			 18:   0  |   1
+			 19:   1  |   0
+			 20:   0  |   1
+
+		NO, no all exchange places found
+
+
+
+Process finished with exit code 0
+
+"""
