858f1eb42732ef1b242aa75ef21ec2e5cadb624a
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-05-21 16:37:27 +0200

    01.Cv E 08.py Sub v03.1

--- 01.Cv/Easy/Submissions/08.HOTELS - Hotels Along the Croatian Coast_Sub.py
+++ 01.Cv/Easy/Submissions/08.HOTELS - Hotels Along the Croatian Coast_Sub.py
@@ -2,7 +2,7 @@
 HOTELS - Hotels Along the Croatian Coast
 https://www.spoj.com/problems/HOTELS/
 
-    There are N hotels along the beautiful Adriatic coast. Each hotel has its value in Euros.
+        There are N hotels along the beautiful Adriatic coast. Each hotel has its value in Euros.
     Sroljo has won M Euros on the lottery. Now he wants to buy a sequence of consecutive hotels, such that the sum
     of the values of these consecutive hotels is as great as possible - but not greater than M.
     You are to calculate this greatest possible total value.
@@ -39,21 +39,21 @@ InputRaw_Str = InputRaw_Str[1:-1]
 def infExtrc(lineInf, lineHtls):
 
     htlNum, mnyNum = list(map(int, lineInf.split()))
-    # print(f"\t\tNum. of hotels: {htlNum}")
-    # print(f"\t\tNum. of money:  {mnyNum}")
-    # print()
+    print(f"\t\tNum. of hotels: {htlNum}")
+    print(f"\t\tNum. of money:  {mnyNum}")
+    print()
 
     Htls = list(map(int, lineHtls.split()))
-    # print(f"\t\tHotels: {Htls}")
-    # print()
+    print(f"\t\tHotels: {Htls}")
+    print()
 
     return htlNum, mnyNum, Htls
 
 
-def fndMaxMny(Htls, mnyNum):
+def fndMaxMny_ForPrt(Htls, mnyNum):
 
-    mnyMax = 0
-    htlMax = []
+    maxMny = 0
+    maxHtls = []
 
     for sH, strHtl in enumerate(Htls):
 
@@ -62,47 +62,165 @@ def fndMaxMny(Htls, mnyNum):
             if sum(Htls[sH:sH + eH + 1]) > mnyNum:
                 break
 
-            elif sum(Htls[sH:sH + eH + 1]) > mnyMax:
-                mnyMax = sum(Htls[sH:sH + eH + 1])
-                htlMax = Htls[sH:sH + eH + 1]
+            elif sum(Htls[sH:sH + eH + 1]) > maxMny:
+                maxMny = sum(Htls[sH:sH + eH + 1])
+                maxHtls = Htls[sH:sH + eH + 1]
 
-            # print(f"\t\t\t{Htls[sH:sH + eH + 1]} ({sum(Htls[sH:sH + eH + 1])})")
+            print(f"\t\t\t{Htls[sH:sH + eH + 1]} ({sum(Htls[sH:sH + eH + 1])})")
 
-        if mnyMax == mnyNum:
+        if maxMny == mnyNum:
             break
-    # print()
+    print()
 
-    return mnyMax, htlMax
+    return maxMny, maxHtls
+
+
+def fndMaxMny_TwoPrt(Htls, mnyNum):
+
+    htlNum = len(Htls)
+
+    strHs = 0
+    endHs = 1
+
+    maxMny = 0
+    maxHtls = []
+
+    while endHs <= htlNum:
+
+        curHtls = Htls[strHs:endHs]
+        sumHtls = sum(curHtls)
+
+        print(f"\t\t\t{strHs} | {endHs} : {Htls[strHs:endHs]} ({sumHtls})")
+
+        if sumHtls < mnyNum:
+            endHs += 1
+
+            if sumHtls > maxMny:
+                maxMny = sumHtls
+                maxHtls = curHtls
+
+        elif sumHtls > mnyNum:
+            strHs += 1
+
+        elif sumHtls == mnyNum:
+            maxMny = mnyNum
+            maxHtls = curHtls
+
+            return maxHtls, maxMny
+
+    return maxHtls, maxMny
 
 
 if __name__ == "__main__":
 
-    # print("Input:")
-    # print(InputRaw_Str)
-    # print()
+    print("Input:")
+    print(InputRaw_Str)
+    print()
 
     InputStr_Lst = InputRaw_Str.split("\n")
 
-    # for lineInf, lineHtls in zip(InputStr_Lst[::2], InputStr_Lst[1::2]):
+    # print(InputStr_Lst)
 
-        # print(f"\t{caseCnt}. Case")
+    caseCnt = 1
+
+    for lineInf, lineHtls in zip(InputStr_Lst[::2], InputStr_Lst[1::2]):
+
+        print(f"\t{caseCnt}. Case")
+        print()
+
+        caseCnt += 1
+
+        htlNum, mnyNum, Htls = infExtrc(lineInf, lineHtls)
+
+        # htlNum, mnyNum = list(map(int, lineInf.split()))
+        # print(f"\t\tNum. of hotels: {htlNum}")
+        # print(f"\t\tNum. of money:  {mnyNum}")
+        # print()
+        #
+        # Htls = list(map(int, lineHtls.split()))
+        # print(f"\t\tHotels: {Htls}")
         # print()
 
-    lineInf = input()
-    lineHtls = input()
+        maxMny, maxHtls = fndMaxMny_TwoPrt(Htls, mnyNum)
 
-    # print()
-    # print("Input:")
-    # print(f"\t{lineInf}")
-    # print(f"\t{lineHtls}")
-    # print()
+        print()
 
-    htlNum, mnyNum, Htls = infExtrc(lineInf, lineHtls)
+        # maxMny, maxHtls = fndMaxMny_ForPrt(Htls, mnyNum)
 
-    mnyMax, htlMax = fndMaxMny(Htls, mnyNum)
+        # maxMny = 0
+        # maxHtls = []
+        #
+        # for sH, strHtl in enumerate(Htls):
+        #
+        #     for eH, endHtl in enumerate(Htls[sH:]):
+        #
+        #         if sum(Htls[sH:sH+eH+1]) > mnyNum:
+        #             break
+        #
+        #         elif sum(Htls[sH:sH+eH+1]) > maxMny:
+        #             maxMny = sum(Htls[sH:sH+eH+1])
+        #             maxHtls = Htls[sH:sH+eH+1]
+        #
+        #         print(f"\t\t\t{Htls[sH:sH + eH + 1]} ({sum(Htls[sH:sH + eH + 1])})")
+        #
+        #     if maxMny == mnyNum:
+        #         break
+        # print()
 
-    # print(f"\tMax money: {mnyMax}")
-    # print(f"\tMax hotels: {htlMax}")
+        print(f"\t\tMax money: {maxMny}")
+        print(f"\t\tMax hotels: {maxHtls}")
 
-    print(mnyMax)
+        print("\n")
+
+
+"""__Output__"""
+"""
+Input:
+5 12
+2 1 3 4 5
+4 9
+7 3 5 6
+
+	1. Case
+
+		Num. of hotels: 5
+		Num. of money:  12
+
+		Hotels: [2, 1, 3, 4, 5]
+
+			0 | 1 : [2] (2)
+			0 | 2 : [2, 1] (3)
+			0 | 3 : [2, 1, 3] (6)
+			0 | 4 : [2, 1, 3, 4] (10)
+			0 | 5 : [2, 1, 3, 4, 5] (15)
+			1 | 5 : [1, 3, 4, 5] (13)
+			2 | 5 : [3, 4, 5] (12)
+
+		Max money: [3, 4, 5]
+		Max hotels: 12
+
+
+	2. Case
+
+		Num. of hotels: 4
+		Num. of money:  9
+
+		Hotels: [7, 3, 5, 6]
+
+			0 | 1 : [7] (7)
+			0 | 2 : [7, 3] (10)
+			1 | 2 : [3] (3)
+			1 | 3 : [3, 5] (8)
+			1 | 4 : [3, 5, 6] (14)
+			2 | 4 : [5, 6] (11)
+			3 | 4 : [6] (6)
+
+		Max money: [3, 5]
+		Max hotels: 8
+
+
+
+Process finished with exit code 0
+
+"""
 
